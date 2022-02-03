a72db8e15f2c5939e0c4aa0dfd2a0a97d3d222e0
anubhav <anubhavajmera25@gmail.com>
2022-02-03 16:14:56 +0530

    created a very basic calculator which perform 4 basic operations used react components concept

---
+++ src/Calc.jsx
@@ -0,0 +1,19 @@
+ function add(a,b){
+    return (a+b)
+  }
+  
+  function substract(c,d){
+    return(c-d)
+  }
+  
+  function mulitply(d,e){
+    return(d*e)
+  }
+  
+  function division(f,g){
+    let div = f/g
+    div = div.toFixed(2)
+    return div
+  }
+  
+  export {add, substract, mulitply, division}
